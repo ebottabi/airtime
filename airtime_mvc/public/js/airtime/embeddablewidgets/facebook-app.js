1089973c646cf9a7f4fb4ef76c3ec38b39023d48
drigato <denise.rigato@sourcefabric.org>
2015-07-28 15:39:40 -0400

    messing around with facebook api

---
+++ airtime_mvc/public/js/airtime/embeddablewidgets/facebook-app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    // to cache SDK
+    /*$.ajaxSetup({ cache: true });
+
+    // imports the SDK from the correct URL for your user’s locale
+    $.getScript('//connect.facebook.net/en_US/sdk.js', function() {
+        // FB object is global so it can be used outside of this function.
+        // Just make sure it exists before using it.
+        FB.init({
+            // do SDK initialization here
+        });
+    });*/
+});
